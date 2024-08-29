f26937046df2a231f3662b59f90e8d9aefe7a4ab
Manjot Singh <immanjot01@gmail.com>
2024-08-30 00:49:38 +0530

    community

--- models/Community.js
+++ models/Community.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const communitySchema = new mongoose.Schema({
   industry: { type: String, required: true, unique: true },
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "Profile" }],
+  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "profileinfos" }],
 });
 
 export default mongoose.model("Community", communitySchema);
